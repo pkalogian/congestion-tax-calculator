088e4fe0b729f0f4d956f51e15bc7750634db578
pkalogian <pkalogiannidis@wideorbit.com>
2021-03-14 21:52:16 +0100

    Changed project structure and added a solution.

---
+++ netcore/src/congestion-tax-calculator-net-core.Tests/CongestionTaxCalculatorTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace congestion_tax_calculator_net_core.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
